5727a3ad68a68c062a6d2f4fe58d347be2322d3e
Ann Schomburg <Ann-Christin.Schomburg@games-academy.de>
2021-02-19 15:29:18 +0100

    Menu

--- Assets/MainMenu.cs
+++ Assets/MainMenu.cs
@@ -1,21 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.Scenemanagement;
+using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
-{
-    public void PlayGame();
+{ 
+
+      public void PlayGame()
     {
     SceneManager.LoadScene("SceneManager.GetActiveScene().buildIndex + 1");
-}
+    }
 
-public void QuitGame();
-{
+    public void QuitGame()
+    {
     Debug.Log("Quit!");
     Application.Quit();
-
-}
+    }
         
    
 }
